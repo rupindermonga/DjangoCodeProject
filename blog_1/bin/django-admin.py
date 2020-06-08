ddb56f9eaefbd51e13549517bca086cd2f79164c
Rupinder <rupindermonga@gmail.com>
2020-06-08 22:29:51 +0530

    Practicing Django code project

---
+++ blog_1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/rupinder/New Volume/Personal/Learning/Django/CoreySchafer/blog_1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
